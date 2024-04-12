f8565bc3deea03c53c061eb8f1a11abb6d7b5488
ArtsyLee <leayonce@gmail.com>
2024-04-13 03:40:48 +0800

    更新网页meta数据

--- src/static/site-metadata.ts
+++ src/static/site-metadata.ts
@@ -10,18 +10,18 @@ interface ISiteMetadataResult {
 }
 
 const data: ISiteMetadataResult = {
-  siteTitle: 'Running Page',
-  siteUrl: 'https://yihong.run',
+  siteTitle: 'ArtsyLee跑步数据',
+  siteUrl: 'https://www.artsylee.cn',
   logo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTtc69JxHNcmN1ETpMUX4dozAgAN6iPjWalQ&usqp=CAU',
   description: 'Personal site and blog',
   navLinks: [
     {
-      name: 'Blog',
-      url: 'https://github.com/yihong0618/gitblog',
+      name: '主页',
+      url: 'https://www.artsylee.cn',
     },
     {
-      name: 'About',
-      url: 'https://github.com/yihong0618/running_page/blob/master/README-CN.md',
+      name: '关于',
+      url: 'https://github.com/ArtsyLee/running/tree/master',
     },
   ],
 };
